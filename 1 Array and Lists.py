c45cfb6811a2037d12c16f3acc9706a866c00665
DhyeyLukhi <dhyeylukhi95@gmail.com>
2025-12-02 16:28:31 +0530

    Added the Arrays and the Lists, video 1

--- 1 Array and Lists.py
+++ 1 Array and Lists.py
@@ -1,4 +1,5 @@
 from array import *
+import numpy as np
 
 a1 = array('i', [1, 2, 3, 4, 5])
 print(a1)
@@ -17,3 +18,10 @@ a1.remove(2)
 print("Remove 2", a1)
 a1.insert(2, 10)
 print("Insert 10 at index 2:", a1)
+
+a = np.array([1, 2, 3, 4, 5])  # 1D array
+print(a)
+b = np.array([[1, 2, 3], [4, 5, 6]])  # 2D array
+print(b)
+c = np.array([[1, 2, 3], [10, 13]])  # 1D array with 2 elements, and both elements are lists
+print(c)
