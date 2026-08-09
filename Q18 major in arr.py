b2a53ff92afcd725e617816cf9af26848d071bbf
DhyeyLukhi <dhyeylukhi95@gmail.com>
2026-08-09 22:29:03 +0530

    a very strange but specially made for this, Boyer–Moore majority vote algorithm works perfectly

--- Q18 major in arr.py
+++ Q18 major in arr.py
@@ -1,18 +1,17 @@
 class Solution:
     def majorityElement(self, nums):
-        major = nums[0]
-        before = len(nums)
-        try:
-            for i in range(len(nums)):
-                if major == nums[i]:
-                    nums.remove(nums[i])
-
-        except Exception as e:
-            pass
-
-        finally:
-            print(major, nums)
-
+        candidate = None
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+            
+        # Phase 2: Verify candidate
+        if nums.count(candidate) > len(nums) // 2:
+            return candidate
+        
 test = Solution()
-ans = test.majorityElement(nums=[2,2,1,1,1,2,2,2])
+ans = test.majorityElement(nums=[6,5,5])
 print(f"Answer is {ans}")
